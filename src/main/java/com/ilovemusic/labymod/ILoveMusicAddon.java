2ac54363e301c4e069f5499225363691cb344c8a
Felix Klauke <info@felix-klauke.de>
2020-03-07 06:30:35 +0100

    Add labymod addon structure and remove test code

---
+++ src/main/java/com/ilovemusic/labymod/ILoveMusicAddon.java
@@ -0,0 +1,23 @@
+package com.ilovemusic.labymod;
+
+import java.util.List;
+import net.labymod.api.LabyModAddon;
+import net.labymod.settings.elements.SettingsElement;
+
+public final class ILoveMusicAddon extends LabyModAddon {
+
+
+  @Override
+  public void onEnable() {
+
+  }
+
+  @Override
+  public void loadConfig() {
+
+  }
+
+  @Override
+  protected void fillSettings(List<SettingsElement> settings) {
+  }
+}
